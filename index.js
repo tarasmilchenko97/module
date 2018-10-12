8065ee51003d7c011b48f9a58ae5ff5e3963c0d2
tarasmilchenko97 <panda.tara.18@gmail.com>
2018-10-12 11:50:30 +0300

    1-5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+
 // Задание 1
+
 function itsMe(a) {
     if (a !== undefined) {
         console.log(`Its me, ${a}`);
@@ -16,35 +18,35 @@ function itsMe(a) {
 
 function compare(num1, num2) {
 
-    if (typeof num1 !== 'number') {
-        console.log(num1,num2);
-    }else if (typeof num2 !== typeof 1){
-        console.log(num1,num2);
-    }else if (typeof num1 !== typeof 1 && typeof num2 !== typeof 1){
-        console.log(num1,num2);
-    } else
-
-    if (num1 < num2) {
-        console.log(`${num1} < ${num2}`);
-    }else if (num1 > num2){
-        console.log(`${num1} > ${num2}`);
-    }else if (num1 === num2) {
-        console.log(`${num1} == ${num2}`);
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' ||
+        typeof num1 !== 'number' && typeof num2 !== 'number') {
+        console.log(String(num1+ ' ' +num2));
+    } else if (num1 < num2) {
+        console.log(String(num1+ ' < ' +num2));
+    } else if (num1 > num2) {
+        console.log(String(num1+ ' > ' +num2));
+    } else if (num1 === num2) {
+        console.log(String(num1+ ' == ' +num2));
     }
 }
-
 
 
 // Задание 3
 
 function row(n1, n2, n3, s4) {
-    let arr1 = [n1, n2, n3];
-    arr1.sort();
-
-
+    let arr = [n1, n2, n3,];
+    let str;
+    function row2(a,b) {
+        if (s4 === '<') {
+            return a - b;
+        } else {
+            return b - a;
+        }
+    }
+    arr.sort(row2);
+    str = arr.join(' ');
+    return str;
 }
-
-alert(row(3, 4, -1,'<'));
 
 
 // Задание 4
@@ -54,40 +56,27 @@ function fact(z) {
 }
 
 
-
 // Задание 5
 
-function matrixDiff(arr1,arr2) {
-    let one= 0;
-    let two = 0;
-    let arrrez = [];
-    let arrrez1 = [];
-    for (let i =0; i <arr1.length; i+=1){
-        one = arrrez[i] + arrrez[i+1];
-
-        for (let k = 0; k <arr1.length; k+=1) {
-            arrrez1[i] = Math.abs(arr1[i][k] - arr2[i][k]);
-            arrrez1.push(arrrez);
+function matrixDiff(arr1, arr2) {
+    let one = 0;
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (arr1[i].length === arr2[i].length) {
+        for (let k = 0; k < arr1.length; k += 1) {
+            one = one + Math.abs(arr1[i][k] - arr2[i][k]);
         }
+            } else {
+               return NaN;
+            }
         }
-
-return one;
-
-
+        return one;
+    }
 
 
+// Задание 6
 
+function strangeSearch(arr) {
 
-
-    // for (let i =0; i <arr2.length; i+=1){
-    //     two = Math.abs(arr2[i] + arr1[i]);
-    //     for (let k = 0; k <arr2.length; k+=1) {
-    //         arr2[i] = Math.abs(arr1[i][k+1] - arr2[i][k+1]);
-    //     }
-    // }
 
 
 }
-
-console.log(matrixDiff([[2,3],[4,5]],
-                        [[2,1],[6,5]],));
